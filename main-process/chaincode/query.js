9c8ce012a3c51ffc61be7a04a885c4ad6d314f26
zhangmin.name <zhangmin@zhangmin.name>
2018-08-28 16:09:35 +0800

    integrated electron with fabric-client

--- main-process/chaincode/query.js
+++ main-process/chaincode/query.js
@@ -1,31 +1,31 @@
-// 'use strict';
-// /*
-// * Copyright IBM Corp All Rights Reserved
-// *
-// * SPDX-License-Identifier: Apache-2.0
-// */
-// /*
-//  * Chaincode query
-//  */
+'use strict';
+/*
+* Copyright IBM Corp All Rights Reserved
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
+/*
+ * Chaincode query
+ */
 
-// var Fabric_Client = require('fabric-client');
-// var path = require('path');
-// var util = require('util');
-// var os = require('os');
+var Fabric_Client = require('fabric-client');
+var path = require('path');
+var util = require('util');
+var os = require('os');
 
-// //
-// var fabric_client = new Fabric_Client();
+//
+var fabric_client = new Fabric_Client();
 
-// // setup the fabric network
-// var channel = fabric_client.newChannel('mychannel');
-// var peer = fabric_client.newPeer('grpc://localhost:7051');
-// channel.addPeer(peer);
+// setup the fabric network
+var channel = fabric_client.newChannel('mychannel');
+var peer = fabric_client.newPeer('grpc://localhost:7051');
+channel.addPeer(peer);
 
-// //
-// var member_user = null;
-// var store_path = path.join(__dirname, 'hfc-key-store');
-// console.log('Store path:'+store_path);
-// var tx_id = null;
+//
+var member_user = null;
+var store_path = path.join(__dirname, 'hfc-key-store');
+console.log('Store path:'+store_path);
+var tx_id = null;
 
 // // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
 // Fabric_Client.newDefaultKeyValueStore({ path: store_path
